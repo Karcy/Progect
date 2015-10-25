df0d5a1899fa37070b4b2e4d290c92bbc753904d
Eugene Chipachenko <echipachenko@gmail.com>
2015-10-25 19:51:12 +0200

    gggg

--- src/ContinueLabel.java
+++ src/ContinueLabel.java
@@ -4,7 +4,7 @@ public class ContinueLabel {
         outer:
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (j > i) {
+                if (j > i) {//ggggg
                     System.out.println();
                     continue outer;
                 }
